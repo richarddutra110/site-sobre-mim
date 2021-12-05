964bcf0ec72e8d3c7faccd9c619ea968a4006b35
Richard Dutra <80215713+richarddutra110@users.noreply.github.com>
2021-12-04 23:48:09 -0300

    Add files via upload

---
+++ js/sask.js
@@ -0,0 +1,17 @@
+var conteudo = document.getElementById('conteudo');
+var botao = document.getElementById('mostre_mais');
+
+botao.onclick = function(){
+
+    if(conteudo.className == "open"){
+
+        conteudo.className = "";
+        botao.innerHTML = "Mostrar Mais";
+    }
+
+    else{
+    conteudo.className = "open";
+    botao.innerHTML = "Mostrar Menos";
+    }
+
+};
